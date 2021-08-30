d732718706785824f0a9911f5d8912b2a2bb839e
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-08-30 15:25:41 +0200

    NavSlice update

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Building my first react native app</Text>
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Building my first react native app</Text>
+      </View>
+    </Provider>
   );
 }
 
