06d7c19d03d8f49429aebe389a031c2fcc06dafe
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-08-31 15:16:25 +0200

    Commit

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
+      <SafeAreaView>
         <HomeScreen />
-      </SafeAreaProvider>
+      </SafeAreaView>
     </Provider>
   );
 }
